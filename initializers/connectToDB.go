012f372e1fc3d4667fa2f88780d1646fcdb64a26
AndrewMbugua <andrewmbugua388@gmail.com>
2025-03-20 11:31:30 +0300

    Adding initializers/connectToDB.go

--- initializers/connectToDB.go
+++ initializers/connectToDB.go
@@ -1,5 +1,22 @@
 package initializers
 
+import (
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"log"
+)
+
+var DB *gorm.DB
+
 func ConnectToDb() {
+
+	// Re-using json_crud_api DB
+	dsn := "host=localhost user=postgres dbname=json_crud_api port=5432 sslmode=disable"
+	var err error
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal("Error opening the database.")
+	}
 
 }
