5abf6a1b4fdcb1139c0407e81c17dd9c0223d937
AndrewMbugua <andrewmbugua388@gmail.com>
2025-03-24 11:16:00 +0300

    Updating middleware/requireAuth.go

--- middleware/requireAuth.go
+++ middleware/requireAuth.go
@@ -3,9 +3,47 @@ package middleware
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v4"
+	"log"
+	"net/http"
+	"os"
+	"time"
 )
 
 func RequireAuth(c *gin.Context) {
-	fmt.Println("In middleware")
+	// Get the cookie off req
+	tokenString, err := c.Cookie("Authorization")
+
+	if err != nil {
+		c.AbortWithStatus(http.StatusUnauthorized)
+	}
+
+	// Decode/validate it
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		// hmacSampleSecret is a []byte containing your secret, e.g. []byte("my_secret_key")
+		return []byte(os.Getenv("SECRET")), nil
+	}, jwt.WithValidMethods([]string{jwt.SigningMethodHS256.Alg()}))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		// Check the expiry
+		if float64(time.Now().Unix()) > claims["exp"].(float64) {
+			c.AbortWithStatus(http.StatusUnauthorized)
+
+		}
+
+		// Find the user with token sub
+
+		// Attach to req
+
+		// Continue
+
+		fmt.Println(claims["foo"], claims["nbf"])
+	} else {
+		c.AbortWithStatus(http.StatusUnauthorized)
+	}
+
 	c.Next()
 }
